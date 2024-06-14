b14f9ecc2ab4d1d65f763f2305e3735d05809899
Ahmad Hadwan <ahmadhadwan2004@gmail.com>
2024-06-14 22:22:08 +0300

    Basing Routing.

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,75 @@
 import { useEffect, useState } from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import CardList from "./components/CardList";
 
 const ID = "1wyoeq_rQbZTOfH8COymYzkNEod8-7FEtogHopVzr0Jw";
-const GID = "0";
+const WANTED_GID = "0";
+const FORSALE_GID = "440879152";
 const URL =
   "https://docs.google.com/spreadsheets/d/" +
   ID +
-  "/gviz/tq?tqx=out:json&tq&gid=" +
-  GID;
+  "/gviz/tq?tqx=out:json&tq&gid=";
 
 function App() {
-  const [cards, setCards] = useState(null);
+  const [wantedCards, setWantedCards] = useState(null);
+  const [cardsForSale, setCardsForSale] = useState(null);
 
   useEffect(() => {
-    fetch(URL)
+    fetch(URL + WANTED_GID)
       .then((response) => response.text())
       .then((responseText) => responseText.match(/(?<="table":).*(?=}\);)/g))
       .then((match) => (match != null ? JSON.parse(match[0]) : null))
-      .then((json) => setCards(json.rows));
+      .then((json) => setWantedCards(json.rows));
+  }, []);
+
+  useEffect(() => {
+    fetch(URL + FORSALE_GID)
+      .then((response) => response.text())
+      .then((responseText) => responseText.match(/(?<="table":).*(?=}\);)/g))
+      .then((match) => (match != null ? JSON.parse(match[0]) : null))
+      .then((json) => setCardsForSale(json.rows));
   }, []);
 
   return (
     <>
-      <div className="card">{cards ? <CardList cards={cards} /> : <></>}</div>
+      <BrowserRouter>
+        <div className="header">
+          <nav>
+            <Link to={"/"}>YuGiOhLb</Link>
+            <ul className="header-nav">
+              <li>
+                <Link to={"/Sale"}>Cards for Sale</Link>
+              </li>
+              <li>
+                <Link to={"/Wanted"}>Wanted Cards</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className="card">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <h1>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
+                  distinctio odio dignissimos ab deleniti corrupti sit quia
+                  delectus, veritatis amet recusandae beatae dolorem dolorum
+                  facilis, quibusdam omnis aspernatur blanditiis accusamus.
+                </h1>
+              }
+            />
+            <Route
+              path="/Sale"
+              element={cardsForSale ? <CardList cards={cardsForSale} /> : <></>}
+            />
+            <Route
+              path="/Wanted"
+              element={wantedCards ? <CardList cards={wantedCards} /> : <></>}
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </>
   );
 }
