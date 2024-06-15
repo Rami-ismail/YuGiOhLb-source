5432a89963ff03a3349dfe54dd574bfbb7c8ea72
Ahmad Hadwan <ahmadhadwan2004@gmail.com>
2024-06-15 05:47:58 +0300

    Cleaned a useless check in Card.tsx, fixed a path in routes.tsx

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,33 +4,33 @@ import { CardSearcher } from "../components/CardSearcher";
 import { signOut } from "../auth/authService";
 import { useAuth } from "../auth/authContext";
 export const AppRoutes = () => {
-    //should be in a sign out component
-    const navigate = useNavigate()
-    const isLoggedIn = useAuth()
-    const onClick = async () => {
-        if (isLoggedIn){
-           await signOut()
-            navigate("/")
-        }
-      };
+  //should be in a sign out component
+  const navigate = useNavigate();
+  const isLoggedIn = useAuth();
+  const onClick = async () => {
+    if (isLoggedIn) {
+      await signOut();
+      navigate("/");
+    }
+  };
   return (
     <Routes>
       <Route element={<SignIn />} path="/" />
       <Route
         path="/home"
         element={
-            <>
+          <>
             <h1>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
-            distinctio odio dignissimos ab deleniti corrupti sit quia delectus,
-            veritatis amet recusandae beatae dolorem dolorum facilis, quibusdam
-            omnis aspernatur blanditiis accusamus.
-          </h1>
-          <button onClick={onClick}>Sign Out</button>
-        </>  
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
+              distinctio odio dignissimos ab deleniti corrupti sit quia
+              delectus, veritatis amet recusandae beatae dolorem dolorum
+              facilis, quibusdam omnis aspernatur blanditiis accusamus.
+            </h1>
+            <button onClick={onClick}>Sign Out</button>
+          </>
         }
       />
-      <Route path="/Search" element={<CardSearcher />} />
+      <Route path="/search" element={<CardSearcher />} />
     </Routes>
   );
 };
